e96c9d6e64c2dfad25943ba41d2299505c939f63
Caio Sguario <cfssguario@gmail.com>
2024-07-19 14:59:54 -0300

    adicionando as sprites da chun-li

---
+++ carrega_animacoes.c
@@ -0,0 +1,12 @@
+#include "carrega_animacoes.h"
+#include <allegro5/allegro5.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_primitives.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void carrega_animacoes(ALLEGRO_BITMAP*** sprites){
+	
+}
